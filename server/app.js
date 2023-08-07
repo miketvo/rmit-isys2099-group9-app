a40b4c5d07ee49ffd5a04def767ce4e062823f56
miketvo <ikxplicitie@gmail.com>
2023-08-07 13:40:27 +0700

    Set up backend linting and formatting with eslint and prettify

--- server/app.js
+++ server/app.js
@@ -4,40 +4,39 @@ const port = 3000;
 
 const mysql = require("mysql");
 const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "isys2099_group9_app",
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "isys2099_group9_app",
 });
 
-
 app.get("/", (req, res) => {
-    connection.connect((err) => {
-        if (err) {
-            console.error("Error connecting to MySQL database: " + err.stack);
-            return;
-        }
-        console.log("Connected to MySQL database as id " + connection.threadId);
+  connection.connect(err => {
+    if (err) {
+      console.error("Error connecting to MySQL database: " + err.stack);
+      return;
+    }
+    console.log("Connected to MySQL database as id " + connection.threadId);
 
-        connection.query(`SELECT * FROM warehouse`, (err, results) => {
-            if (err) {
-                console.error("error: " + err.stack);
-                return;
-            }
+    connection.query(`SELECT * FROM warehouse`, (err, results) => {
+      if (err) {
+        console.error("error: " + err.stack);
+        return;
+      }
 
-            res.send("Hello, world\n" + results[0].warehouse_name);
-        });
-
-        connection.end((err) => {
-            if (err) {
-                console.error("Error connecting to MySQL database: " + err.stack);
-                return;
-            }
-            console.log("Connection closed");
-        });
+      res.send("Hello, world\n" + results[0].warehouse_name);
     });
+
+    connection.end(err => {
+      if (err) {
+        console.error("Error connecting to MySQL database: " + err.stack);
+        return;
+      }
+      console.log("Connection closed");
+    });
+  });
 });
 
 app.listen(port, () => {
-    console.log(`Backend API Server listening on port ${port}`);
+  console.log(`Backend API Server listening on port ${port}`);
 });
