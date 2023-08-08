f85f98e97129b7b994507b946920705060b7d0a6
miketvo <ikxplicitie@gmail.com>
2023-08-09 06:00:48 +0700

    Added mock data for lazada_user and related tables

--- database/mysql/init/mock_data.sql
+++ database/mysql/init/mock_data.sql
@@ -1,1 +1,12 @@
 USE isys2099_group9_app;
+
+
+INSERT INTO lazada_user (username, password_hash)
+VALUES ('tony', ''),
+       ('loi', '');
+
+INSERT INTO buyer (username)
+VALUES ('tony');
+
+INSERT INTO seller (username, shop_name)
+VALUES ('loi', 'loc');
