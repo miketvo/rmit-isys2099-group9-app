55458b50729082ac1efe8ab54f27e881e86408a4
miketvo <ikxplicitie@gmail.com>
2023-08-09 05:43:20 +0700

    Added create_users_roles.sql

---
+++ database/mysql/create_users_roles.sql
@@ -0,0 +1,8 @@
+CREATE USER IF NOT EXISTS 'isys2099_group9_app_whadmin_user'@'localhost'
+    IDENTIFIED WITH 'caching_sha2_password' BY 'jlAfD3sBdpJvz0phULIq4CAaAMIIoGNA';
+
+CREATE USER IF NOT EXISTS 'isys2099_group9_app_seller_user'@'localhost'
+    IDENTIFIED WITH 'caching_sha2_password' BY 'IAAX8qFR3bdXkQrb4dM77QGgV9FxRGap';
+
+CREATE USER IF NOT EXISTS 'isys2099_group9_app_buyer_user'@'localhost'
+    IDENTIFIED WITH 'caching_sha2_password' BY 'gxQJCza0eADkT5AKmeE865ZN8p1nBsar';
