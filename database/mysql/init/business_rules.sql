1b98c54238530a7b9372042e6ebc09d0a03b6b87
miketvo <ikxplicitie@gmail.com>
2023-08-12 09:22:34 +0700

    More template code in business_rules.sql

--- database/mysql/init/business_rules.sql
+++ database/mysql/init/business_rules.sql
@@ -2,23 +2,45 @@ USE isys2099_group9_app;
 
 
 /*
- int so_move_product(product_id int, move_quantity int, to_warehouse: int)
+ int so_move_product(product_id int, move_quantity int, from_warehouse: int, to_warehouse: int)
+
+ Returns:
+    -1 on rollback
+    0 on successful commit
+    1 on data not exists
+    2 on illegal argument value
  */
 DROP PROCEDURE IF EXISTS sp_move_product;
 DELIMITER $$
-CREATE FUNCTION sp_move_product(IN product_id INT, IN move_quantity INT, IN to_warehouse INT) RETURNS INT
-this_func:
+CREATE FUNCTION sp_move_product(
+    IN product_id INT,
+    IN move_quantity INT,
+    IN from_warehouse INT,
+    IN to_warehouse INT
+) RETURNS INT
 BEGIN
     DECLARE _rollback BOOL DEFAULT 0;
     DECLARE CONTINUE HANDLER FOR SQLEXCEPTION SET _rollback = 1;
     START TRANSACTION;
+
+    -- Checks for early termination
+    IF move_quantity < 1 THEN RETURN 2; END IF;
+
+    SELECT count(*) INTO @exist_product FROM product WHERE id = product_id FOR UPDATE;
+    IF @exist_product = 0 THEN RETURN 1; END IF;
+
+    SELECT count(*) INTO @exist_from_warehouse FROM warehouse WHERE id = from_warehouse FOR UPDATE;
+    IF @exist_from_warehouse = 0 THEN RETURN 1; END IF;
+
+    SELECT count(*) INTO @exist_to_warehouse FROM warehouse WHERE id = to_warehouse FOR UPDATE;
+    IF @exist_to_warehouse = 0 THEN RETURN 1; END IF;
 
 
 
     -- Commit or Rollback
     IF _rollback THEN
         ROLLBACK;
-        RETURN 1;
+        RETURN -1;
     ELSE
         COMMIT;
         RETURN 0;
