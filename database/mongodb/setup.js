d8decd977430497d1688669cbea776031d460e85
miketvo <ikxplicitie@gmail.com>
2023-08-23 10:37:05 +0700

    Successful connection to database

--- database/mongodb/setup.js
+++ database/mongodb/setup.js
@@ -1,6 +1,6 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
-const uri = "mongodb://localhost:27017";  // Putting localhost here will NOT work
+const uri = "mongodb://127.0.0.1:27017";  // Putting localhost here will NOT work
 const client = new MongoClient(uri,  {
     serverApi: {
       version: ServerApiVersion.v1,
