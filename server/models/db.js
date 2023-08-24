5923fca8b9616fd3f3b5ac17329e9576e1869ca5
dolelongan <dolelongan@gmail.com>
2023-08-25 00:14:38 +0700

    changing connection to pool for multiple concurrent connections

--- server/models/db.js
+++ server/models/db.js
@@ -13,17 +13,18 @@ class Database {
     }
 
     initDB() {
-        this.connection = mysql.createConnection({
+        this.pool = mysql.createPool({
             host: process.env.DB_HOST,
             user: process.env.DB_USER_ADM,
             password: process.env.DB_PASS_ADM,
             database: process.env.MYSQL_DB,
+            connectionLimit: process.env.CONNECTION_LIMIT,
         });
     }
 
     connectDB() {
         return new Promise((resolve, reject) => {
-            this.connection.connect(err => {
+            this.pool.connect(err => {
                 if (err) {
                     console.error("Error connecting to MySQL database: " + err.stack);
                     reject(err);
