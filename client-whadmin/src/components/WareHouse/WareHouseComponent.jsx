b6028ef0381505684b12daa3089e02e519801676
dolelongan <dolelongan@gmail.com>
2023-08-28 23:44:13 +0700

    fix WareHouseComponent

--- client-whadmin/src/components/WareHouse/WareHouseComponent.jsx
+++ client-whadmin/src/components/WareHouse/WareHouseComponent.jsx
@@ -10,22 +10,35 @@ import {BiSearch} from "react-icons/bi"
 import {IoAddOutline} from "react-icons/io5"
 import { IconSetting } from "../../utils/IconSettings"
 
-import { getDataAPI } from "../../api/fetchAPI";
+import {getDataAPI} from "../../api/fetchAPI"
+import PopUp from "./PopUp/PopUp"
 const WareHouseComponent = () => {
   
   useEffect(() => {
-    async function fetchData() {
-      const data = await getDataAPI("api/warehouse");
-      setWareHouseData(data);
-    }
+    const fetchData = async () => {
+      try {
+          const result = await await getDataAPI('warehouse');
+          setWareHouseData(result)
+      } catch (error) {
+          // Handle the error
+          console.error('Error fetching data:', error);
+      }
+    };
+
     fetchData();
   }, []);
 
   useEffect(() => {
-    async function fetchData() {
-      const data = await getDataAPI("api/product");
-      setProductData(data);
-    }
+    const fetchData = async () => {
+      try {
+          const result = await await getDataAPI('product');
+          setProductData(result)
+      } catch (error) {
+          // Handle the error
+          console.error('Error fetching data:', error);
+      }
+    };
+
     fetchData();
   }, []);
   
@@ -82,45 +95,51 @@ const WareHouseComponent = () => {
   
 
   return (
-    <div className="warehouse_wrapper mt-3">
-      <div className="container">
-        <div className="warehouse_header d-flex flex-rows justify-content-between align-items-center">
-          <div className="warehouse_tabs">
-            <ul className="tab_header">
-              {
-                WareHouseTabsMap.map((tab) => (
-                  <li className={`tab_item ${wareHouseTabs === tab.id ? "active" : ""}`} key={tab.id} onClick={() => setWareHouseTab(tab.id)}>
-                    <span className="text-capitalize">{tab.id}</span>
-                  </li>
-                ))
-              }
-            </ul>
-          </div>
-
-          <div className="">
-            <div className="input-group">
-              <div className="input-group-btn search-panel">
-                  <div className="btn btn-default dropdown-toggle" id="filterDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <span id="search_concept">Filter by</span> <span className="caret"></span>
-                  </div>
-                  <ul className="dropdown-menu" aria-labelledby="filterDropdown">
-                    <li><a href="#contains">Contains</a></li>
-                    <li><a href="#its_equal">It&apos;s equal</a></li>
-                    <li><a href="#greather_than">Greather than &gt;</a></li>
-                    <li><a href="#less_than">Less than &lt;    </a></li>
-                    <li className="divider"></li>
-                    <li><a href="#all">Anything</a></li>
-                  </ul>
-              </div>
-              <input type="hidden" name="search_param" value="all" id="search_param" />         
-              <input type="text" className="form-control" name="x" placeholder="Search term..." />
-              <span className="input-group-btn">
-                  <button className="btn btn-default" type="button">
-                    {IconSetting(<BiSearch/>, "black", "16px")}
-                  </button>
-              </span>
+    <Fragment>
+      <div className="warehouse_wrapper mt-3">
+        <div className="container">
+          <div className="warehouse_header d-flex flex-rows justify-content-between align-items-center">
+            <div className="warehouse_tabs">
+              <ul className="tab_header">
+                {
+                  WareHouseTabsMap.map((tab) => (
+                    <li className={`tab_item ${wareHouseTabs === tab.id ? "active" : ""}`} key={tab.id} 
+                    onClick={() => {
+                      setWareHouseTab(tab.id);
+                      setPopUpState(prevState => ({...prevState, type: tab.id}))
+                      }}
+                    >
+                      <span className="text-capitalize">{tab.id}</span>
+                    </li>
+                  ))
+                }
+              </ul>
             </div>
-          </div>
+
+            <div className="">
+              <div className="input-group">
+                <div className="input-group-btn search-panel">
+                    <div className="btn btn-default dropdown-toggle" id="filterDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      <span id="search_concept">Filter by</span> <span className="caret"></span>
+                    </div>
+                    <ul className="dropdown-menu" aria-labelledby="filterDropdown">
+                      <li><a href="#contains">Contains</a></li>
+                      <li><a href="#its_equal">It&apos;s equal</a></li>
+                      <li><a href="#greather_than">Greather than &gt;</a></li>
+                      <li><a href="#less_than">Less than &lt;    </a></li>
+                      <li className="divider"></li>
+                      <li><a href="#all">Anything</a></li>
+                    </ul>
+                </div>
+                <input type="hidden" name="search_param" value="all" id="search_param" />         
+                <input type="text" className="form-control" name="x" placeholder="Search term..." />
+                <span className="input-group-btn">
+                    <button className="btn btn-default" type="button">
+                      {IconSetting(<BiSearch/>, "black", "16px")}
+                    </button>
+                </span>
+              </div>
+            </div>
 
             <div className="">
               <button className="btn btn-success" onClick={handlePopUpForm}>
@@ -145,7 +164,9 @@ const WareHouseComponent = () => {
         </div>
       </div>
 
-  );
-};
+      { popUpState.state === true && <PopUp compData={PopUpData} compFunction={PopUpFunction}/>}
+    </Fragment>
+  )
+}
 
 export default WareHouseComponent;
