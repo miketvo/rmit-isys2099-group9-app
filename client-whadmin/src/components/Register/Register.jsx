45e51584f119a4ad5fca3b9fd5a0850c8fe1fbd6
Phan Thanh Loi <phanthanhloi22112001@gmail.com>
2023-08-28 16:51:30 +0700

    update changed for client-whadmin

--- client-whadmin/src/components/Register/Register.jsx
+++ client-whadmin/src/components/Register/Register.jsx
@@ -1,39 +1,33 @@
 import { useState } from "react";
-import { useNavigate, Link } from "react-router-dom";
+import {useNavigate, Link} from "react-router-dom"
 
 const RegisterComponent = () => {
   const initialState = {
     username: "",
-    password: "",
+    password: ""
   };
   const [registerState, setLoginState] = useState(initialState);
-  const [confirmPassword, setConfirmPassword] = useState("");
-  const { username, password } = registerState;
+  const [confirmPassword, setConfirmPassword] = useState("")
+  const {username, password} = registerState;
 
   const navigate = useNavigate();
 
   // Functions
-  const handleChangeInput = e => {
-    const { name, value } = e.target;
-    setLoginState(prevState => ({ ...prevState, [name]: value }));
-  };
+  const handleChangeInput = (e) => {
+    const {name, value} = e.target;
+    setLoginState(prevState => ({...prevState, [name]: value}))
+  }
 
-  const handleLoginUser = e => {
+  const handleLoginUser = (e) => {
     e.preventDefault();
 
     if (confirmPassword === password) {
-      console.log(registerState);
-      localStorage.setItem(
-        "firstLogin",
-        JSON.stringify({
-          username: username,
-          accessToken: "2937569823659816723946",
-        }),
-      );
+      console.log(registerState)
+      localStorage.setItem("firstLogin", JSON.stringify({username: username, accessToken: "2937569823659816723946"}));
 
-      navigate("/");
+      navigate("/")
     }
-  };
+  }
 
   return (
     <div className="login_wrapper container">
@@ -46,64 +40,38 @@ const RegisterComponent = () => {
             </div>
 
             <div className="form-floating mb-4">
-              <input
-                type="text"
-                className="form-control"
-                id="floatingInput"
-                placeholder=""
-                name="username"
-                value={username}
-                onChange={handleChangeInput}
-              />
+              <input type="text" className="form-control" id="floatingInput" placeholder=''
+              name='username' value={username} onChange={handleChangeInput}/>
               <label htmlFor="floatingInput">Username</label>
             </div>
 
             <div className="form-floating mb-4">
-              <input
-                type="password"
-                className="form-control"
-                id="floatingPassword"
-                placeholder=""
-                name="password"
-                value={password}
-                onChange={handleChangeInput}
-              />
+              <input type="password" className="form-control" id="floatingPassword" placeholder=''
+              name='password' value={password} onChange={handleChangeInput}/>
               <label htmlFor="floatingPassword">Password</label>
             </div>
 
             <div className="form-floating mb-4">
-              <input
-                type="password"
-                className="form-control"
-                id="floatingPassword"
-                placeholder=""
-                name="confirmPassword"
-                value={confirmPassword}
-                onChange={e => setConfirmPassword(e.target.value)}
-              />
+              <input type="password" className="form-control" id="floatingPassword" placeholder=''
+              name='confirmPassword' value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}/>
               <label htmlFor="floatingPassword">Confirm Password</label>
             </div>
-
-            <button
-              type="submit"
-              className="btn btn-outline-primary w-50 mt-4 px-4 "
-            >
-              Register
-            </button>
+            
+            <button type="submit" className="btn btn-outline-primary w-50 mt-4 px-4 ">Register</button>
           </form>
 
           <div className="">
             <p className="mb-0">
               Have already an account?
-              <Link to="/login" className="fw-bold ms-1">
-                Sign In
-              </Link>
+              <Link to="/login" className="fw-bold ms-1">Sign In</Link>
             </p>
           </div>
+          
         </div>
       </div>
+
     </div>
-  );
+  )
 };
 
 export default RegisterComponent;
--- client-whadmin/src/components/WareHouse/Items/WareHouse.jsx
+++ client-whadmin/src/components/WareHouse/Items/WareHouse.jsx
@@ -1,63 +1,63 @@
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 
 // Icon Imported
-import { BiEdit } from "react-icons/bi";
-import { ImBin2 } from "react-icons/im";
+import {BiEdit} from "react-icons/bi"
+import {ImBin2} from "react-icons/im"
 
-const WareHouse = ({ data, compFunction }) => {
-  const { handleDeleteData } = compFunction;
+const WareHouse = ({data, compFunction}) => {
+    const {handleDeleteData} = compFunction;
 
-  return (
-    <div className="warehouse_table mt-4">
-      <table className="table table-striped table-hover">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Warehouse Name</th>
-            <th>Volume</th>
-            <th>Province</th>
-            <th>City</th>
-            <th>District</th>
-            <th>Street</th>
-            <th>Street Number</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data?.map(item => {
-            return (
-              <tr key={item.id}>
-                <th scope="row">{item.id}</th>
-                <td>{item.warehouse_name}</td>
-                <td>{item.volume}</td>
-                <td>{item.province}</td>
-                <td>{item.city}</td>
-                <td>{item.district}</td>
-                <td>{item.street}</td>
-                <td>{item.street_number}</td>
-                <td>
-                  <span className="btn btn-primary">
-                    <BiEdit />
-                  </span>
-                  <span
-                    className="btn btn-warning ms-2"
-                    onClick={() => handleDeleteData(item.id, "warehouse")}
-                  >
-                    <ImBin2 />
-                  </span>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </div>
-  );
-};
+    return (
+        <div className="warehouse_table mt-4">
+            <table className="table table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Warehouse Name</th>
+                        <th>Volume</th>
+                        <th>Province</th>
+                        <th>City</th>
+                        <th>District</th>
+                        <th>Street</th>
+                        <th>Street Number</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        data?.map((item) => {
+                            return(
+                                <tr key={item.id}>
+                                    <th scope="row">{item.id}</th>
+                                    <td>{item.warehouse_name}</td>
+                                    <td>{item.volume}</td>
+                                    <td>{item.province}</td>
+                                    <td>{item.city}</td>
+                                    <td>{item.district}</td>
+                                    <td>{item.street}</td>
+                                    <td>{item.street_number}</td>
+                                    <td>
+                                        <span className="btn btn-primary">
+                                            <BiEdit />
+                                        </span>
+                                        <span className="btn btn-warning ms-2"
+                                        onClick={() => handleDeleteData(item.id, "warehouse")}>
+                                            <ImBin2 />
+                                        </span>
+                                    </td>
+
+                                </tr>
+                            )
+                        })
+                    }
+                </tbody>
+            </table>
+        </div>
+    )
+}
 
 WareHouse.propTypes = {
-  data: PropTypes.arrayOf(
-    PropTypes.shape({
+    data: PropTypes.arrayOf(PropTypes.shape({
       id: PropTypes.number.isRequired,
       warehouse_name: PropTypes.string.isRequired,
       volume: PropTypes.number,
@@ -65,12 +65,13 @@ WareHouse.propTypes = {
       city: PropTypes.string,
       district: PropTypes.string,
       street: PropTypes.string,
-      street_number: PropTypes.number,
-    }),
-  ).isRequired,
-  compFunction: PropTypes.shape({
-    handleDeleteData: PropTypes.func.isRequired,
-  }).isRequired,
-};
+      street_number: PropTypes.string
+    })).isRequired,
+    compFunction: PropTypes.shape({
+        handleDeleteData: PropTypes.func.isRequired
+    }).isRequired
+}
 
-export default WareHouse;
+
+
+export default WareHouse
